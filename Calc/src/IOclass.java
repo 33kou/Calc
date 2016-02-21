c48ab83db23bdea61a214703ea069761a399e5a9
33kou <okke.3wa6ra@ezweb.ne.jp>
2016-02-21 16:02:12 +0900

    コンストラクタ、getNameメソッドの追加

--- Calc/src/IOclass.java
+++ Calc/src/IOclass.java
@@ -1,4 +1,15 @@
 
 public class IOclass {
-
+	
+	String name;
+	int number;
+	public IOclass(String n,int num)
+	{
+		name = n;
+		number = num;
+	}
+	public String getName()
+	{
+		return name;
+	}
 }
