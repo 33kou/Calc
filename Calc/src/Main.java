4301491ab481ae2283b62b8f8dbdae84429496f7
Koichi <k.gsm726@gmail.com>
2016-02-21 16:07:10 +0900

    Main

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-	 
-
+		String string="string";
+		int i=30;
+		IOclass iOclass =new IOclass(string,i);
+		System.out.println(iOclass.getName());
+		System.out.println(iOclass.getNumber());
 	}
 
 }
